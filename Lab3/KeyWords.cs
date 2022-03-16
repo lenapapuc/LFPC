5fbc20e1fbdd5aa31ebe4a80a58cafad046d73f1
lenapapuc <74040842+lenapapuc@users.noreply.github.com>
2022-03-16 11:45:18 +0200

    commit lab3

---
+++ Lab3/KeyWords.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    public static class KeyWords
+    {
+        public const string Digits = "1234567890";
+        public const string Letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        public static readonly Dictionary<string, string> Keywords = new() { 
+            { "Function", "FUNCTION" }, 
+            { "if", "IF" }, 
+            { "elseif", "ELSEIF" }, 
+            { "else", "ELSE" },
+            { "VAR", "VAR"},
+            {"END", "END"},
+            {"DO", "DO"},
+            {"return", "RETURN"}
+        };
+
+        public static readonly Dictionary<string, string> PredefinedFunctions = new()
+        {
+            { "Write", "CONSOLEWRITE" },
+            { "Main", "MAIN" },
+        };
+    }
+}
