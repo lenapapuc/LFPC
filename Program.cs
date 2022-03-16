968057781c9b3925a8a3fe0e06a4e47614db879e
lenapapuc <74040842+lenapapuc@users.noreply.github.com>
2022-03-16 11:40:25 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿using lexer;
+
+var program = File.ReadAllText(@"C:\Users\User\RiderProjects\Solution1\Lab3\BMI.txt");
+Lexer lexer = new Lexer(program);
+lexer.Scan();
+lexer.PrintResult();
---
+++ TokenType.cs
@@ -0,0 +1,36 @@
+﻿namespace Lab3;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+    public static class TokenType
+    {
+        public const string Integer = "INTEGER";
+        public const string Name = "NAME";
+        public const string Keyword = "KEYWORD";
+        public const string PredefinedFunction = "PREDEFINED_FUNCTION";
+        public const string StringStart = "STRING_START";
+        public const string String = "STRING";
+        public const string StringEnd = "STRING_END";
+        public const string LeftParantheses = "LEFT_PARANTHESES";
+        public const string RightParantheses = "RIGHT_PARANTHESES";
+        public const string Comma = "COMMA";
+        public const string Plus = "PLUS";
+        public const string Minus = "MINUS";
+        public const string Multiplication = "MULTIPLICATION";
+        public const string Division = "DIVISION";
+        public const string Greater = "GREATER";
+        public const string GreaterOrEqual = "GREATER_OR_EQUAL";
+        public const string Less = "LESS";
+        public const string LessOrEqual = "LESS_OR_EQUAL";
+        public const string Not = "NOT";
+        public const string NotEquals = "NOT_EQUALS";
+        public const string Equals = "EQUALS";
+        public const string Assignment = "ASSIGNMENT";
+        public const string Or = "OR";
+        public const string And = "AND";
+        public const string Float = "FLOAT";
+    }
