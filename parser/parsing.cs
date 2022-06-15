8fb71c6c90806166d20d6fcd4edcb5009ef8fe76
lenapapuc <74040842+lenapapuc@users.noreply.github.com>
2022-06-15 13:26:36 +0300

    new parsing method

--- parser/parsing.cs
+++ parser/parsing.cs
@@ -8,59 +8,79 @@ public class Parsing
 {
     public void ParsingMethod()
     {
-        var input = "abcdeabcccd$";
+        var input = "$abcdeabcccd";
         Stack<string> stack = new Stack<string>();
         stack.Push("$");
-        stack.Push(input[0].ToString());
-        var newinput = input.Remove(0,1);
+        stack.Push(input[^1].ToString());
+        var newinput = input.Remove(input.Length-1,1);
         Console.WriteLine(newinput);
+        var stackstring = string.Empty;
 
-        while (  stack.Peek() !="S" )
+        while (  stackstring !="S$" || newinput != "$" )
         {
-            string SPrelation = matrix[stack.Peek()][newinput[0].ToString()];
-            if (SPrelation is "<" or "=")
+           
+            string SPrelation = matrix[newinput[^1].ToString()][stack.Peek()];
+            if (SPrelation is ">" or "=")
             {
-                stack.Push(newinput[0].ToString());
-                newinput = newinput.Remove(0,1);
+                stack.Push(newinput[^1].ToString());
+                newinput = newinput.Remove(newinput.Length-1,1);
             }
-            else if (SPrelation == ">")
+            else if (SPrelation == "<")
             {
 
                 StringBuilder handle = new StringBuilder();
                 handle.Append(stack.Peek());
                 stack.Pop();
-                
-                while (matrix[stack.Peek()][handle.ToString()[^1].ToString()] is not "<" or ">")
+                //Console.WriteLine(handle.ToString());
+
+                while (matrix[handle.ToString()[^1].ToString()][stack.Peek()] is not "<" && matrix[handle.ToString()[^1].ToString()][stack.Peek()] is not ">" )
                 {
                     handle.Append(stack.Peek());
                     stack.Pop();
-                    
-                }
+                  // newinput = newinput.Remove(newinput.Length - 1, 1);
 
+                } //Console.WriteLine(handle.ToString());*/
                 string ne= String.Empty;
-                if (handle.ToString().Length > 1)
+               if (handle.ToString().Length > 1)
                 {
                     var charst = handle.ToString().ToCharArray();
                     var c = charst.Reverse();
                     ne = new string(c.ToArray());
                 }
-                else ne = handle.ToString();
+                
+                 ne = handle.ToString();
+               //Console.WriteLine(ne);
                 var myKEY = Transitions.FirstOrDefault(x => x.Value.Contains(ne)).Key;
+              //  Console.WriteLine(myKEY);
                 StringBuilder neweststring = new StringBuilder();
-                neweststring.Append(myKEY);
+                
                 neweststring.Append(newinput);
+                neweststring.Append(myKEY);
                 newinput = neweststring.ToString();
-               
-            }
 
-          /*  Console.Write("This is the stack: ");
+            }
+            else 
+            {
+               Console.WriteLine("String not accepted!"); 
+               break;
+            }
+            
+            if (stackstring == "S$" && newinput == "$")
+            {
+                Console.WriteLine("String accepted!");
+            }
+          
+            /*Console.Write("This is the stack: ");
             foreach(string str in stack)
             {
                 // Using ToString() method
                 Console.Write(str.ToString());
             }
-                Console.WriteLine();*/
-            Console.WriteLine( newinput);
+         Console.WriteLine();*/
+             stackstring = string.Join("", stack);
+             Console.WriteLine(newinput);
+             //Console.WriteLine("THIS IS STACK STRING " + stackstring);
+             if(stackstring == "S$" && newinput == "$") Console.WriteLine("String accepted");
         } 
     }
 }
